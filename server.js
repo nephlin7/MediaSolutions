10e1f6e224636e2d713a8501adad522b8e912935
gerald7 <nephlin7@hotmail.com>
2018-03-10 12:47:39 -0600

    modify server.js

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+
+
+app.get('/*', function (req, res) {
+    res.sendfile(path.join(__dirname + '/dist/index.html'));
+    
+})
